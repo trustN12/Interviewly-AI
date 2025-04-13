d30c58fb5740c4d95e8d050f61cbf29b43a7b50e
NABARUN <nbiswas100@outlook.com>
2025-04-14 02:53:19 +0530

    updated

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-<<<<<<< HEAD
+  
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-=======
-  output: 'standalone',
->>>>>>> 953fc3ea1e7eedd765d1363f118e5534c034fb35
 };
 
 export default nextConfig;
